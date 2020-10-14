386645c98b5fae74d7b1dcfb9003c65d77f5c083
TheoHappy <teodore473@gmail.com>
2020-10-14 18:53:07 +0300

    first commit

---
+++ app/src/main/java/com/example/reminderapp/Database/EventDao.java
@@ -0,0 +1,22 @@
+package com.example.reminderapp.Database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface EventDao {
+
+    @Insert
+    void insertAll(EntityClass entityClass);
+
+    @Query("SELECT * FROM myTable")
+    List<EntityClass> getAllData();
+
+    @Delete
+    public void delete(EntityClass entityClass);
+
+}
---
+++ app/src/main/java/com/example/reminderapp/UpdateActivity.java
@@ -0,0 +1,23 @@
+package com.example.reminderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.reminderapp.Database.DatabaseClass;
+
+public class UpdateActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+
+
+    }
+
+}
